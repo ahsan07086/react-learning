5a092430fb1696d98ecd9aed253ee35322acefaa
Nakib <ahsannakib335@gmail.com>
2025-11-23 00:40:22 -0800

    Order-grid separation

--- Ecommerce-web/src/Ecommerce pages/Order.jsx
+++ Ecommerce-web/src/Ecommerce pages/Order.jsx
@@ -1,9 +1,8 @@
 import { Header } from '../HeaderComponents/Header';
-import { useEffect, useState, Fragment } from 'react';
-import dayjs from 'dayjs';
-import { monocent } from '../utilities/monocent';
+import { useEffect, useState } from 'react';
 import './Order.css';
 import axios from 'axios';
+import { Ordergrid } from './Order-grid';
 export function Order({cartitem}){
     let [orderitem,setOrderitem] = useState([]);
     useEffect(()=>{
@@ -20,79 +19,7 @@ export function Order({cartitem}){
 
                <div className="orders-page">
                 <div className="page-title">Your Orders</div>
-
-                <div className="orders-grid">
-                {
-                    orderitem.map((order)=>{
-                    return(
-                    <div key={order.id} className="order-container">
-
-                    <div className="order-header">
-                        <div className="order-header-left-section">
-                        <div className="order-date">
-                            <div className="order-header-label">Order Placed:</div>
-                            <div>{dayjs(order.orderTimeMs).format('MMMM D')}</div>
-                        </div>
-                        <div className="order-total">
-                            <div className="order-header-label">Total:</div>
-                            <div>{monocent(order.totalCostCents)}</div>
-                        </div>
-                        </div>
-
-                        <div className="order-header-right-section">
-                        <div className="order-header-label">Order ID:</div>
-                        <div>{order.id}</div>
-                        </div>
-                    </div>
-
-                <div className="order-details-grid">
-                    {
-                        order.products.map((orderproduct)=>{
-                          return(
-                            <Fragment key={orderproduct.product.id}>
-                            <div className="product-image-container">
-                             <img src={orderproduct.product.image} />
-                            </div>
-
-                            <div className="product-details">
-                            <div className="product-name">
-                                {orderproduct.product.name}
-                            </div>
-                            <div className="product-delivery-date">
-                                Arriving on: {dayjs(orderproduct.estimatedDeliveryTimeMs).format('dddd, MMMM D')}
-                            </div>
-                            <div className="product-quantity">
-                                Quantity: {orderproduct.quantity}
-                            </div>
-                            <button className="buy-again-button button-primary">
-                                <img className="buy-again-icon" src="images/icons/buy-again.png" />
-                                <span className="buy-again-message">Add to Cart</span>
-                            </button>
-                            </div>
-
-                            <div className="product-actions">
-                            <a href="/track">
-                                <button className="track-package-button button-secondary">
-                                Track package
-                                </button>
-                            </a>
-                            </div>
-                            
-                            </Fragment>
-                          )
-                        })
-                    }
-                    
-
- 
-                </div>
-                </div>
-                        )
-                     })
-                }
-                    
-
-            </div>
+                 <Ordergrid orderitem={orderitem}/>
                </div>
         </>
      )
